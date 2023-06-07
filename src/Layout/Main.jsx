2c286b94ee5d7c4ed0a448e525cfd5ee6cdc0aa4
Wazed Biplob <www.biplob@gmail.com>
2023-06-07 20:49:25 +0600

    Navbar UI completed

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Shared/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
